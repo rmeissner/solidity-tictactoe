b915ddc47c8ed747a123f7b85f46843a5a48d5c1
Richard Meissner <RiMe@gmx.net>
2017-12-28 22:43:16 +0100

    More tests

--- test/ticTacToe.js
+++ test/ticTacToe.js
@@ -1,6 +1,7 @@
 const util = require('util');
 const utils = require('./utils')
 const { getParamFromTxEvent, assertRejects, logGasUsage } = utils
+const { wait, waitUntilBlock } = require('@digix/tempo')(web3);
 
 const TicTacToe = artifacts.require("./TicTacToe.sol")
 
@@ -12,7 +13,7 @@ async function setupGame(accounts, ticTacToe) {
 
   // Create game
   var tx = await ticTacToe.create({value: web3.toWei(1, 'ether')})
-  logGasUsage("Created game", tx)
+  logGasUsage("creating a game", tx)
   var gameId = getParamFromTxEvent(tx, "gameIndex")
   assert.equal(gameId, 1)
   assert.equal(await ticTacToe.gamesCounter(), 2)
@@ -25,6 +26,7 @@ async function setupGame(accounts, ticTacToe) {
   assertRejects(ticTacToe.join(1, {from: accounts[1], value: web3.toWei(2, 'ether')}), "Should only create game if exactly 1 ether is paid")
   assertRejects(ticTacToe.join(2, {from: accounts[1], value: web3.toWei(1, 'ether')}), "Should not be able to join non-existing game")
   await ticTacToe.join(1, {from: accounts[1], value: web3.toWei(1, 'ether')})
+  assertRejects(ticTacToe.join(1, {from: accounts[1], value: web3.toWei(1, 'ether')}), "Should not be able to join a game again")
   assert.equal(await web3.eth.getBalance(ticTacToe.address).toNumber(), web3.toWei(2, 'ether'))
   assert.notEqual(await ticTacToe.getGameInfo(1), 0)
   assert.equal(await ticTacToe.currentGameState(1), 1)
@@ -32,7 +34,7 @@ async function setupGame(accounts, ticTacToe) {
 }
 
 async function makeMove(accounts, ticTacToe, currentPlayer, previousMove, moves, game, field, expectedPlayerReset = false) {
-  logGasUsage("Made move number " + (parseInt(moves) + 1), await ticTacToe.makeMove(game, field, {from: accounts[currentPlayer - 1]}))
+  logGasUsage("move number " + (parseInt(moves) + 1), await ticTacToe.makeMove(game, field, {from: accounts[currentPlayer - 1]}))
   if (await ticTacToe.currentGameState(1) == 1) {
     // Player should only change if the game doesn't ends
     currentPlayer = (currentPlayer % 2) + 1
@@ -43,7 +45,7 @@ async function makeMove(accounts, ticTacToe, currentPlayer, previousMove, moves,
   assert.equal(await ticTacToe.currentPlayerIndex(game), currentPlayer)
 
   var lastMove = await ticTacToe.lastMove(game)
-  assert.ok("last move should be after previous move: " + lastMove + " vs " + previousMove, previousMove < lastMove)
+  assert.ok(previousMove <= lastMove, "last move should be after previous move: " + lastMove + " vs " + previousMove)
   moves++
   assert.equal(moves, await ticTacToe.currentMoves(game))
 
@@ -76,7 +78,7 @@ contract('TicTacToe', function(accounts) {
         assert.equal(await ticTacToe.senderPlayerIndex(1), 1)
         assert.equal(await web3.eth.getBalance(ticTacToe.address).toNumber(), web3.toWei(1, 'ether'))
 
-        assertRejects(ticTacToe.join(1, web3.toWei(1, 'ether')), "Should not be able to join own game")
+        assertRejects(ticTacToe.join(1, {value: web3.toWei(1, 'ether')}), "Should not be able to join own game")
         assertRejects(ticTacToe.makeMove(1, 4), "Should not be able to make a move without a second player")
         assertRejects(ticTacToe.punishCurrentPlayer(1), "Should not be able to punish before game start")
         assertRejects(ticTacToe.cancel(1, {from: accounts[1]}), "Other users should not be able to cancel game")
@@ -89,6 +91,7 @@ contract('TicTacToe', function(accounts) {
 
     it('Schould create, join and end a game with a winner',  async () => {
         await setupGame(accounts, ticTacToe)
+        assert.equal(await ticTacToe.senderPlayerIndex(1, {from: accounts[3]}), 0, "Unknown player should have index 0")
         var currentPlayer = await ticTacToe.currentPlayerIndex(1)
         var lastMove = await ticTacToe.lastMove(1)
         var moves = await ticTacToe.currentMoves(1)
@@ -100,11 +103,44 @@ contract('TicTacToe', function(accounts) {
         var account1Balance = await web3.eth.getBalance(accounts[1]).toNumber()
 
         // Start playing
+        assertRejects(ticTacToe.makeMove(1, 9, {from: accounts[currentPlayer - 1]}), "Should not be able to make a move if to an non-existing field")
         assertRejects(ticTacToe.makeMove(1, 4, {from: accounts[(currentPlayer % 2)]}), "Should not be able to make a move if it is not my turn")
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 4)
-        assertRejects(ticTacToe.makeMove(1, 4, {from: accounts[(currentPlayer % 2)]}), "Should not be able to make a move on a occupied field")
+        assertRejects(ticTacToe.makeMove(1, 4, {from: accounts[(currentPlayer - 1)]}), "Should not be able to make a move on a occupied field")
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 0)
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 2)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 6)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 5)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 3)
+        if (currentPlayer == 2) {
+          assert.ok(account1Balance < web3.eth.getBalance(accounts[1]).toNumber(), "Winner should get all")
+          assert.ok(account0Balance > web3.eth.getBalance(accounts[0]).toNumber(), "Looser should get nothing")
+        } else {
+          assert.ok(account0Balance < web3.eth.getBalance(accounts[0]).toNumber(), "Winner should get all")
+          assert.ok(account1Balance > web3.eth.getBalance(accounts[1]).toNumber(), "Looser should get nothing")
+        }
+        assert.equal(await ticTacToe.currentGameState(1), 2)
+        assert.equal(await web3.eth.getBalance(ticTacToe.address).toNumber(), 0)
+    })
+
+    it('Schould create, join and end a game with a winner on last move',  async () => {
+        await setupGame(accounts, ticTacToe)
+        var currentPlayer = await ticTacToe.currentPlayerIndex(1)
+        var lastMove = await ticTacToe.lastMove(1)
+        var moves = await ticTacToe.currentMoves(1)
+        assert.notEqual(currentPlayer, 0)
+        assert.notEqual(lastMove, 0)
+        assert.equal(moves, 0)
+
+        var account0Balance = await web3.eth.getBalance(accounts[0]).toNumber()
+        var account1Balance = await web3.eth.getBalance(accounts[1]).toNumber()
+        // Start playing
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 0)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 8)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 7)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 1)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 2)
+        var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 4)
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 6)
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 5)
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 3)
@@ -142,6 +178,40 @@ contract('TicTacToe', function(accounts) {
         var [currentPlayer, lastMove, moves] = await makeMove(accounts, ticTacToe, currentPlayer, lastMove, moves, 1, 7, true)
         assert.ok(account0Balance < web3.eth.getBalance(accounts[0]).toNumber(), "Stake should be refunded")
         assert.ok(account1Balance < web3.eth.getBalance(accounts[1]).toNumber(), "Stake should be refunded")
+        assert.equal(await ticTacToe.currentGameState(1), 2)
+        assert.equal(await web3.eth.getBalance(ticTacToe.address).toNumber(), 0)
+    })
+
+    it('Schould create, join and kick a player',  async () => {
+        await setupGame(accounts, ticTacToe)
+        var currentPlayer = await ticTacToe.currentPlayerIndex(1)
+        var lastMove = await ticTacToe.lastMove(1)
+        var moves = await ticTacToe.currentMoves(1)
+        assert.notEqual(currentPlayer, 0)
+        assert.notEqual(lastMove, 0)
+        assert.equal(moves, 0)
+
+        assertRejects(ticTacToe.cancel(1), "Should not be able to cancel a game after start")
+        assertRejects(ticTacToe.punishCurrentPlayer(1, {from: accounts[(currentPlayer % 2)]}), "Should not be able to punish a player right after game start")
+
+        var account0Balance = await web3.eth.getBalance(accounts[0]).toNumber()
+        var account1Balance = await web3.eth.getBalance(accounts[1]).toNumber()
+
+        await wait(3500, 1)
+        assertRejects(ticTacToe.punishCurrentPlayer(1, {from: accounts[(currentPlayer % 2)]}), "Should not be able to punish a player right after game start")
+        assert.ok(!(await ticTacToe.canCurrentPlayerBePunished(1, {from: accounts[(currentPlayer % 2)]})), "Player should not be punihsable before 1 hour")
+        await wait(101, 1)
+        assert.ok(await ticTacToe.canCurrentPlayerBePunished(1, {from: accounts[(currentPlayer % 2)]}), "Player should be punihsable after 1 hour")
+        assertRejects(ticTacToe.punishCurrentPlayer(1, {from: accounts[(currentPlayer - 1)]}), "Current player should not be able to punish himself")
+        await ticTacToe.punishCurrentPlayer(1, {from: accounts[(currentPlayer % 2)]})
+
+        if (await ticTacToe.currentPlayerIndex(1) == 2) {
+          assert.ok(account1Balance < web3.eth.getBalance(accounts[1]).toNumber(), "Kicking player should get all")
+          assert.ok(account0Balance > web3.eth.getBalance(accounts[0]).toNumber(), "Kicked player should get nothing")
+        } else {
+          assert.ok(account0Balance < web3.eth.getBalance(accounts[0]).toNumber(), "Kicking player should get all")
+          assert.ok(account1Balance > web3.eth.getBalance(accounts[1]).toNumber(), "Kicked player should get nothing")
+        }
         assert.equal(await ticTacToe.currentGameState(1), 2)
         assert.equal(await web3.eth.getBalance(ticTacToe.address).toNumber(), 0)
     })
